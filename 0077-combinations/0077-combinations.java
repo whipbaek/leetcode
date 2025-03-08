e06a1dd780f61063aa4d62957da38e18007fc8bc
백종인 <75191916+whipbaek@users.noreply.github.com>
2025-03-08 22:38:37 +0900

    Time: 19 ms (49.24%), Space: 95 MB (19.41%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -3,11 +3,11 @@ class Solution {
     public List<List<Integer>> answer = new ArrayList<>();
     public List<List<Integer>> combine(int n, int k) {
         List<Integer> list = new ArrayList<>();
-        func(list, 1, 0, n, k);
+        func(list, 1, n, k);
         return answer;
     }
 
-    public void func (List<Integer> list, int val, int idx, int n, int k){
+    public void func (List<Integer> list, int val, int n, int k){
 
         if(list.size() == k){
             answer.add(new ArrayList<>(list));
@@ -16,8 +16,8 @@ class Solution {
         
         for(int i=val; i<=n; i++){
             list.add(i);
-            func(list, i+1, idx+1, n, k);
-            list.remove(idx);
+            func(list, i+1, n, k);
+            list.remove(list.size() - 1);
         }
     }
 
